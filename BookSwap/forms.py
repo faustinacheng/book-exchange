89287295c2ee09315811df078a65422b18546648
Faustina Cheng <chengfaustina@gmail.com>
2021-08-03 07:24:32 +0800

    Style and customize flash messages

--- BookSwap/forms.py
+++ BookSwap/forms.py
@@ -3,10 +3,12 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from BookSwap.models import User
 
+
 class LoginForm(FlaskForm):
-    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
-    password = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[DataRequired()])
+    password = PasswordField(label='Password:', validators=[DataRequired()])
     submit = SubmitField(label='Log In')
+
 
 class RegisterForm(FlaskForm):
     def validate_email_address(self, email_address_to_check):
@@ -15,7 +17,11 @@ class RegisterForm(FlaskForm):
             raise ValidationError('Email Address already exists! Please try a different email address')
 
     name = StringField(label='Name:', validators=[Length(min=2, max=50), DataRequired()])
-    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
-    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
-    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(message='Email address is invalid.'), DataRequired()])
+    password1 = PasswordField(label='Password:',
+                              validators=[Length(min=6, message='Password must be at least 6 characters long.'),
+                                          DataRequired()])
+    password2 = PasswordField(label='Confirm Password:',
+                              validators=[EqualTo(fieldname='password1', message='Passwords do not match.'),
+                                          DataRequired()])
     submit = SubmitField(label='Sign Up')
--- BookSwap/routes.py
+++ BookSwap/routes.py
@@ -1,6 +1,6 @@
-from BookSwap import app, login_manager, db
+from BookSwap import app, db
 from flask import render_template, url_for, flash, redirect
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required, current_user
 from BookSwap.models import User
 from BookSwap.forms import LoginForm, RegisterForm
 
@@ -15,19 +15,19 @@ def login_page():
     form = LoginForm()
     if form.validate_on_submit():
         attempted_user = User.query.filter_by(email_address=form.email_address.data).first()
-        if attempted_user and attempted_user.check_password(attempted_password = form.password.data):
+        if attempted_user and attempted_user.check_password(attempted_password=form.password.data):
             login_user(attempted_user)
-            flash(f'You are Successfully logged in as { attempted_user.name }')
+            flash(f'Welcome back, { attempted_user.name }!', category='success')
             return redirect(url_for('home_page'))
-        else: 
-            flash('Username and Password don\'t match! Please try again', category='')
+        else:
+            flash('Email address or password is not correct! Please try again', category='error')
 
     return render_template('login.html', form=form)
 
 @app.route('/logout')
 def logout_page():
     logout_user()
-    flash('You have been Logged Out!', category='')
+    flash('We\'ll miss you, come back soon!', category='info')
     return redirect(url_for('home_page'))
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -40,10 +40,10 @@ def register_page():
         db.session.add(user_to_create)
         db.session.commit()
         login_user(user_to_create)
-        flash(f"Account created successfully! You are now logged in as {user_to_create.name}", category='')
+        flash(f'Account created successfully! You are now logged in as {user_to_create.name}', category='success')
         return redirect(url_for('home_page'))
     if form.errors != {}: #If there are errors from the validations
         for err_msg in form.errors.values():
-            flash(f'There was an error with creating a user: {err_msg}', category='')
+            flash(f'{err_msg[0]}', category='error')
 
     return render_template('register.html', form=form)
