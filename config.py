565317747644e5973ff01ea43df38b875f48f45b
Faustina Cheng <chengfaustina@gmail.com>
2021-08-11 04:48:23 +0800

    Add config

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'af532f4f0eae5f1b561af82a'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-            'postgres://', 'postgresql://') or 'postgresql:///bookswap'
+            'postgres://', 'postgresql://') or 'postgresql:///' + os.path.join(basedir, 'bookswap')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
